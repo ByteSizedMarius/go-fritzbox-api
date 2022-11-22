c49f1f196f7ca7627f08a78ace7a54751e32bab9
ByteSizedMarius <marius.schmalz@outlook.de>
2022-11-22 20:37:09 +0100

    simplified clientlist parsing

--- clientlist.go
+++ clientlist.go
@@ -4,52 +4,56 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
-	"regexp"
 	"strings"
 )
 
+// Note: I have since found out about landevice:settings/landevice/list but I do not currently want to implement it
+// It would break alot of things, have little benefit for me over the current method and I'm short on time currently
+// PRs welcome, but please don't break API (meaning it should be implemented additional to current method, current method can be marked as deprecated however)
+
 type Clientlist struct {
 	Rootuid string `json:"rootuid"`
-	Devices []struct {
-		Devid     string `json:"devid"`
-		Stateinfo struct {
-			GuestOwe        bool `json:"guest_owe"`
-			Active          bool `json:"active"`
-			Guest           bool `json:"guest"`
-			Online          bool `json:"online"`
-			Blocked         bool `json:"blocked"`
-			Realtime        bool `json:"realtime"`
-			Notallowed      bool `json:"notallowed"`
-			InternetBlocked bool `json:"internetBlocked"`
-		} `json:"stateinfo,omitempty"`
-		Profile    Profile
-		Devtype    string   `json:"devtype"`
-		Dist       int      `json:"dist"`
-		Parent     string   `json:"parent"`
-		Category   string   `json:"category"`
-		Ownentry   bool     `json:"ownentry"`
-		UID        string   `json:"UID"`
-		Conn       string   `json:"conn"`
-		Master     bool     `json:"master"`
-		Ipinfo     []string `json:"ipinfo"`
-		Updateinfo struct {
-			State string `json:"state"`
-		} `json:"updateinfo"`
-		Gateway  bool `json:"gateway"`
-		Nameinfo struct {
-			Name string `json:"name"`
-		} `json:"nameinfo,omitempty"`
-		Children []interface{} `json:"children"`
-		Conninfo []struct {
-			Speed   string `json:"speed"`
-			SpeedTx int    `json:"speed_tx"`
-			SpeedRx int    `json:"speed_rx"`
-			Desc    string `json:"desc"`
-		} `json:"conninfo"`
-	} `json:"devices"`
+	Devices []Device
 }
 
-// GetCLientList returns a Clientlist-Object containing a devicelist
+type Device struct {
+	Devid     string `json:"devid"`
+	Stateinfo struct {
+		GuestOwe        bool `json:"guest_owe"`
+		Active          bool `json:"active"`
+		Guest           bool `json:"guest"`
+		Online          bool `json:"online"`
+		Blocked         bool `json:"blocked"`
+		Realtime        bool `json:"realtime"`
+		Notallowed      bool `json:"notallowed"`
+		InternetBlocked bool `json:"internetBlocked"`
+	} `json:"stateinfo,omitempty"`
+	Profile    Profile
+	Devtype    string   `json:"devtype"`
+	Dist       int      `json:"dist"`
+	Parent     string   `json:"parent"`
+	Category   string   `json:"category"`
+	Ownentry   bool     `json:"ownentry"`
+	UID        string   `json:"UID"`
+	Conn       string   `json:"conn"`
+	Master     bool     `json:"master"`
+	Ipinfo     []string `json:"ipinfo"`
+	Updateinfo struct {
+		State string `json:"state"`
+	} `json:"updateinfo"`
+	Gateway  bool `json:"gateway"`
+	Nameinfo struct {
+		Name string `json:"name"`
+	} `json:"nameinfo,omitempty"`
+	Children []interface{} `json:"children"`
+	Conninfo []struct {
+		Speed   string `json:"speed"`
+		SpeedTx int    `json:"speed_tx"`
+		SpeedRx int    `json:"speed_rx"`
+		Desc    string `json:"desc"`
+	} `json:"conninfo"`
+}
+
 func (c *Client) GetCLientList() (clients Clientlist, err error) {
 	if err = c.checkExpiry(); err != nil {
 		return
@@ -70,51 +74,43 @@ func (c *Client) GetCLientList() (clients Clientlist, err error) {
 		return
 	}
 
-	if len(body) > 10 {
-		body = strings.Split(body[strings.Index(body, "{"):], "\n")[0]
-		body = body[strings.Index(body, "{\"rootuid") : strings.Index(body, "}}},")+3]
+	// get json from response
+	body = strings.Split("{\"rootuid\""+strings.Split(body, "{\"rootuid\"")[1], "}},\"nexus\"")[0] + "}}"
 
-		// Devices zum Array machen
-		body = strings.Replace(body, "\"devices\":{", "\"devices\":[", 1)
-		body = body[:len(body)-3] + "]}"
-
-		var re = regexp.MustCompile(`("landevice[0-9]{3,4}":{)|("[0-9]{1,4}":{)`)
-		x := re.FindStringIndex(body)
-
-		for x != nil {
-			preIndex := x[0]
-			sufIndex := x[1]
-
-			// Bis zum lan-device unverändert
-			pre := body[:preIndex]
-
-			// Den landevice-string zur Variable machen
-			pre += "{\"devid\":" + body[preIndex:sufIndex]
-			pre = pre[:len(pre)-2] + ","
-
-			// Den Rest des Bodys wieder anhängen
-			body = pre + body[sufIndex:]
-
-			x = re.FindStringIndex(body)
-		}
-
-		// sometimes the uids are an integer for some reason
-		re = regexp.MustCompile(`"UID":[0-9]*,`)
-		uid := re.FindAllStringIndex(body, -5)
-		for i, uidInt := range uid {
-			currUid := body[uidInt[0]+(i*2) : uidInt[1]+(i*2)]
-			body = strings.Replace(body, currUid, "\"UID\":\""+strings.Split(strings.Split(currUid, ":")[1], ",")[0]+"\",", 1)
-		}
-
-		// Conninfo macht kein Sinn wenns leer ist, anderes Format als wenns voll ists
-		body = strings.ReplaceAll(body, "conninfo\":[[]]", "conninfo\":[]")
-
-		// Ende des Arrays setzen
-		body = body[:len(body)-2] + "}]}"
-
-		clients = Clientlist{}
-		err = json.Unmarshal([]byte(body), &clients)
+	// get rid of top level
+	tmp := map[string]json.RawMessage{}
+	err = json.Unmarshal([]byte(body), &tmp)
+	if err != nil {
+		return
 	}
+
+	// rootuid
+	err = json.Unmarshal(tmp["rootuid"], &clients.Rootuid)
+	if err != nil {
+		return
+	}
+
+	// parse devices
+	clients.Devices = []Device{}
+	err = json.Unmarshal(tmp["devices"], &tmp)
+	if err != nil {
+		return
+	}
+
+	for _, v := range tmp {
+		var d Device
+
+		err = json.Unmarshal(v, &d)
+		if err != nil {
+			// ignore errors because they will happen for devices not relevant here (dect)
+			continue
+		}
+
+		if d.Nameinfo.Name != "" && d.UID != "" {
+			clients.Devices = append(clients.Devices, d)
+		}
+	}
+	err = nil
 
 	return
 }
